3fb6bd8f55229de9dfdfbf67bfa820e999ce0e86
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:43:25 +0200

    Adds division and multiplication operation

--- calculator.cpp
+++ calculator.cpp
@@ -9,3 +9,8 @@ double Calculator::Sub (double a, double b)
 {
 	return Add (a, -b);
 }
+
+int Calculator::Mul (double a, double b)
+{
+    return a * b + 0.5;
+}
--- calculator.h
+++ calculator.h
@@ -3,10 +3,16 @@
 
 class Calculator
 {
+<<<<<<< HEAD
 	public:
 		double Add (double, double);
 		double Sub (double, double);
 
+=======
+    public:
+        int Add (int, int);
+        int Mul (double, double);
+>>>>>>> e0ea21b (add a multiplication operation)
 };
 
 #endif//CALCULATOR_H
