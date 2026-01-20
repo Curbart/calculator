8aaa4242502a573c49c1205ba216b7b81b70b8d0
Ruslan Soloviov <soloviov.ruslan@lll.kpi.ua>
2026-01-20 01:02:24 +0100

    Fixes bugs

--- calculator.cpp
+++ calculator.cpp
@@ -1,15 +1,11 @@
-#include "calculator.h"
+#include "calculator.h"#include "calculator.h"
 
-int Calculator::Add (double a, double b)
+double Calculator::Add (double a, double b)
 {
-<<<<<<< HEAD
-    return a + b;
-=======
 	return a + b + 0.5;
->>>>>>> 4ad40a3 (fix truncation error)
 }
 
-int Calculator::Sub (double a, double b)
+double Calculator::Sub (double a, double b)
 {
-    return Add (a, -b);
+	return Add (a, -b);
 }
