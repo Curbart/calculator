534e9b02dc5d5dcae4f84e7700bf25841879fde5
Ruslan Soloviov <soloviov.ruslan@lll.kpi.ua>
2026-01-20 01:39:40 +0100

    Creates patch files and squashes commits

--- calculator.cpp
+++ calculator.cpp
@@ -5,7 +5,14 @@ double Calculator::Add (double a, double b)
 	return a + b + 0.5;
 }
 
+<<<<<<< HEAD
 double Calculator::Sub (double a, double b)
 {
 	return Add (a, -b);
 }
+=======
+int Calculator::Sub (int a, int b)
+{
+    return Add (a, -b);
+}
+>>>>>>> 976f691 (add a subtraction operation)
--- calculator.h
+++ calculator.h
@@ -3,9 +3,15 @@
 
 class Calculator
 {
+<<<<<<< HEAD
 	public:
 		int Add (double, double);
 		int Sub (double, double);
+=======
+    public:
+        int Add (int, int);
+        int Sub (int, int);
+>>>>>>> 976f691 (add a subtraction operation)
 };
 
 #endif//CALCULATOR_H
